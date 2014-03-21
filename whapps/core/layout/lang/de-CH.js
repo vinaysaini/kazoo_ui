05db734015ae52605fd652dcb063992b0bdf02ce
root <root@vinay.viithiisys.com>
2014-03-21 12:44:55 +0530

    change logo

--- whapps/core/layout/lang/de-CH.js
+++ whapps/core/layout/lang/de-CH.js
@@ -2,7 +2,7 @@ window.translate['layout'] = $.extend(true, {}, window.translate['layout'], {
 	welcome_to_the: "Willkommen im",
 	platform: "Plattform.",
 	learn_more: "MEHR",
-	powered_by: "Präsentiert von 2600Hz",
+	powered_by: "Präsentiert von Webovs",
 	please_wait: "Bitte warten Sie ..."
 	
 });
--- whapps/core/layout/lang/nl.js
+++ whapps/core/layout/lang/nl.js
@@ -2,7 +2,7 @@ window.translate['layout'] = {
 	welcome_to_the: "Welkom bij het",
 	platform: "platform.",
 	learn_more: "MEER INFORMATIE",
-	powered_by: "Ontworpen door 2600hz",
+	powered_by: "Ontworpen door Webovs",
 	please_wait: "Een moment geduld..."
 	
 };
